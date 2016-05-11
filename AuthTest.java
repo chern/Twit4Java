5d9e0fabad0c5339562eb3e288ae782da135ce5c
Nishan D'Souza <nishand@gmail.com>
2016-05-10 17:48:28 -0700

    OAuth Fully Working
    OMG it actually works

--- AuthTest.java
+++ AuthTest.java
@@ -24,86 +24,28 @@ public class AuthTest
 {
     public static void main(String[] args) {
         String testStatus="Hello from twitter4j";
-
+        String consumerKey = "LqFDdgq7SurJdoQeAtBiDmC8p";
+        String consumerSecret = "GDdnMzYJyLddVFgdeXET9I0sHzQFYMGgozIrcTiJzDcTSflogo";
+        String accessToken = "729714377562030082-FtbarB7pQ6BbMK8589vPpoiVgFBMV0i";
+        String accessSecret = "o1GgPnihASydGh7jgMQEFncw7DQp0hGfHC9BcvpXL4A0a";
         ConfigurationBuilder cb = new ConfigurationBuilder();
-
-        //the following is set without accesstoken- desktop client
         cb.setDebugEnabled(true);
-        cb.setOAuthConsumerKey("LqFDdgq7SurJdoQeAtBiDmC8p");
-        cb.setOAuthConsumerSecret("GDdnMzYJyLddVFgdeXET9I0sHzQFYMGgozIrcTiJzDcTSflogo");
-        cb.setOAuthAccessToken("729714377562030082-FtbarB7pQ6BbMK8589vPpoiVgFBMV0i");
-        cb.setOAuthAccessTokenSecret("o1GgPnihASydGh7jgMQEFncw7DQp0hGfHC9BcvpXL4A0a");
-
+        cb.setOAuthConsumerKey(consumerKey);
+        cb.setOAuthConsumerSecret(consumerSecret);
+        cb.setOAuthAccessToken(accessToken);
+        cb.setOAuthAccessTokenSecret(accessSecret);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+        System.setProperty("twitter4j.http.useSSL", "false");
+        AccessToken a = new AccessToken(accessToken, accessSecret);
+        twitter.setOAuthAccessToken(a);
         try {
-            TwitterFactory tf = new TwitterFactory(cb.build());
-            Twitter twitter = tf.getInstance();
-
-            try {
-                System.out.println("-----");
-
-                // get request token.
-                // this will throw IllegalStateException if access token is already available
-                // this is oob, desktop client version
-                RequestToken requestToken = twitter.getOAuthRequestToken(); 
-
-                System.out.println("Got request token.");
-                System.out.println("Request token: " + requestToken.getToken());
-                System.out.println("Request token secret: " + requestToken.getTokenSecret());
-                
-                System.out.println("|-----");
-
-                AccessToken accessToken = null;
-
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-                while (accessToken == null) {
-                    System.out.println("Open the following URL and grant access to your account:");
-                    System.out.println(requestToken.getAuthorizationURL());
-                    System.out.print("Enter the PIN(if available) and hit enter after you granted access.[PIN]:");
-                    String pin = br.readLine();
-
-                    try {
-                        if (pin.length() > 0) {
-                            accessToken = twitter.getOAuthAccessToken(requestToken, pin);
-                        } else {
-                            accessToken = twitter.getOAuthAccessToken(requestToken);
-                        }
-                    } catch (TwitterException te) {
-                        if (401 == te.getStatusCode()) {
-                            System.out.println("Unable to get the access token.");
-                        } else {
-                            te.printStackTrace();
-                        }
-                    }
-                }
-                System.out.println("Got access token.");
-                System.out.println("Access token: " + accessToken.getToken());
-                System.out.println("Access token secret: " + accessToken.getTokenSecret());
-
-            } catch (IllegalStateException ie) {
-                // access token is already available, or consumer key/secret is not set.
-                if (!twitter.getAuthorization().isEnabled()) {
-                    System.out.println("OAuth consumer key/secret is not set.");
-                    System.exit(-1);
-                }
-            }
-            /*
-            Status status = twitter.updateStatus(testStatus);
-
+            Status status = twitter.updateStatus("HI");
             System.out.println("Successfully updated the status to [" + status.getText() + "].");
-
-            System.out.println("ready exit");
-
-            System.exit(0);
-            */
-        } catch (TwitterException te) {
+        }
+        catch(TwitterException te) {
+            System.out.println("FAIL!");
             te.printStackTrace();
-            System.out.println("Failed to get timeline: " + te.getMessage());
-            System.exit(-1);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-            System.out.println("Failed to read the system input.");
-            System.exit(-1);
         }
     }
 }
